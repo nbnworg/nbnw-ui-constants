3e5f3a422c33457a60d973711e51c1f58d335ffc
Hardik Mehta <imt_2021039@iiitm.ac.in>
2024-07-12 15:59:41 +0530

    Changed src to news folder

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // index.js
-import { supportedCategories } from './newsDefinitions/constants/categories';
-import { API_PATHS } from './newsDefinitions/enums/apiPaths';
-import { ComponentSize } from './newsDefinitions/enums/componentSize';
-import { ComponentColorType } from './newsDefinitions/enums/componentColorType';
-import { QueryType } from './newsDefinitions/enums/queryType';
-import * as x from './newsDefinitions/interfaces/post';
-import * as y from './newsDefinitions/interfaces/componentType';
-import * as z from './newsDefinitions/interfaces/metaData';
-import * as a from './newsDefinitions/interfaces/postMetaData';
-import * as b from './newsDefinitions/interfaces/postEditor';
-import * as c from './newsDefinitions/interfaces/postFrontend';
+import { supportedCategories } from './news/constants/categories';
+import { API_PATHS } from './news/enums/apiPaths';
+import { ComponentSize } from './news/enums/componentSize';
+import { ComponentColorType } from './news/enums/componentColorType';
+import { QueryType } from './news/enums/queryType';
+import * as x from './news/interfaces/post';
+import * as y from './news/interfaces/componentType';
+import * as z from './news/interfaces/metaData';
+import * as a from './news/interfaces/postMetaData';
+import * as b from './news/interfaces/postEditor';
+import * as c from './news/interfaces/postFrontend';
 
 const Metadata = z.Metadata;
 const PostMetadata = a.PostMetadata;
